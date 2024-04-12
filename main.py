a4c4b72cfdce4b4e219707dd57adfb011f52e3b0
Tim-McCann <tjm8679@gmail.com>
2024-04-11 20:58:03 -0500

    added bookbot funcitonality

---
+++ main.py
@@ -0,0 +1,50 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    chars_dict = get_char_dict(text)
+    chars_sorted_list = char_dict_to_sorted_list(chars_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_word_count(text):
+    count = len(text.split())
+    return count
+
+def get_char_dict(text):
+    char_dict= {}
+    lowered_text = text.lower()
+    for char in lowered_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def char_dict_to_sorted_list(chars_dict):
+    sorted_list = []
+    for ch in chars_dict:
+        sorted_list.append({"char": ch, "num": chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+main()
